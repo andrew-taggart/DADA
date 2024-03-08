b38a9eb7107b2f65b62c2429edfa06442798f64c
andrew-taggart <andrew.taggart53@gmail.com>
2024-03-08 15:56:08 -0500

    backend task 1

---
+++ dada-app/src/data/controllers/goalsController.jsx
@@ -0,0 +1,68 @@
+const { Goal } = require('../models')
+
+const getAllGoals = async (req, res) => {
+    try {
+        const goal = await Goal.find()
+        res.json(goal)
+    } catch (error) {
+        return res.status(500).send(error.message)
+    }
+}
+
+const getGoalById = async (req, res) => {
+    try {
+        const { id } = req.params
+        const goal = await UseGoalr.findById(id)
+        if (goal) {
+            return res.json(goal)
+        }
+        return res.status(404).send('goal with the specified ID does not exists');
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
+}
+
+const createGoal = async (req, res) => {
+    try {
+        const goal = await new Goal(req.body)
+        await goal.save()
+        return res.status(201).json({
+            goal
+        })
+    } catch (error) {
+        return res.status(500).json({error: error.message})
+    }
+}
+
+const updateGoal = async (req, res) => {
+    try {
+        let { id } = req.params;
+        let goal = await Goal.findByIdAndUpdate(id, req.body, { new: true })
+        if (goal) {
+            return res.status(200).json(tv)
+        }
+        throw new Error("goal not found")
+    } catch (error) {
+        return res.status(500).send(error.message)
+    }
+}
+
+const deleteGoal = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const deleted = await TV.findByIdAndDelete(id)
+        if (deleted) {
+            return res.status(200).send("goal deleted")
+        }
+        throw new Error("goal item not found")
+    } catch (error) {
+        return res.status(500).send(error.message)
+    }
+}
+module.exports = {
+    getAllGoals,
+    getGoalById,
+    createGoal,
+    updateGoal,
+    deleteGoal,
+}
---
+++ dada-app/src/data/seeds/milestoneSeed.jsx
@@ -0,0 +1,37 @@
+const db = require('../db')
+const { Goal, User, Milestone } = require('../models')
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+
+const main = async () => {
+    const userId = await User.find({ type: 'admin' })
+    const goalId = await User.find({ type: 'admin' })
+    listMilestones = [
+        {
+            userID: userId[0]._id,
+            goalID: goalId[0]._id,
+            taskName: 'styling',
+            dueDate : new Date(2024, 2, 9),
+            accomplished: true,
+            isActive: true,
+            description: 'Making it pretty'
+        },
+        {
+            userID: userId[0]._id,
+            goalID: goalId[0]._id,
+            taskName: 'backend',
+            dueDate : new Date(2024, 2, 9),
+            accomplished: false,
+            isActive: true,
+            description: 'Making it pretty'
+        },
+    ]
+    await Milestone.insertMany(listMilestones)
+    console.log('Created Milestones')
+
+}
+const run = async () => {
+    await main()
+    db.close()
+}
+
+run()
