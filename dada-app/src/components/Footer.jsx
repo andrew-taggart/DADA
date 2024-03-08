b582f11bde6943d61eb1e005059362cd0d525d6c
toddlf705 <toddlf705@gmail.com>
2024-03-08 15:07:45 -0500

    created header, footer, and landing page with designs

--- dada-app/src/components/Footer.jsx
+++ dada-app/src/components/Footer.jsx
@@ -1,7 +1,34 @@
+import github from '../assets/github.png'
+import linkedin from '../assets/linkedin.png'
+
 const Footer = () => {
     return (
-        <div className='home-container'>
-         <h1>Footer</h1>
+        <div className='footer-container'>
+            <div className='profile-container'>
+                <div className="profile" id="amid">
+                    Amid Martinez 
+                    <div><a href=""><img src={github}></img></a>
+                    <a href=""><img src={linkedin}></img></a></div>
+                </div>
+                <div className="profile" id="andrew">
+                    Andrew Taggart
+                    <div><a href=""><img src={github}></img></a>
+                    <a href=""><img src={linkedin}></img></a></div>
+                </div>
+                <div className="profile" id="daisy">
+                    Daisy Ban
+                    <div><a href=""><img src={github}></img></a>
+                    <a href=""><img src={linkedin}></img></a></div>
+                </div>
+                <div className="profile" id="denesh">
+                    Denesh Anandathasan
+                    <div><a href=""><img src={github}></img></a>
+                    <a href=""><img src={linkedin}></img></a></div>
+                </div>
+            </div>
+            <div className="contact-us">
+                <h5>Contact Us</h5>
+            </div>
         </div>
     )
 }
