0660602f8a2ee5480a1bd49b201baf1e32fe7db9
Denesh Anandathasan <denesh555@gmail.com>
2024-03-15 09:08:48 -0400

    pull commit

--- dada-app/src/App.jsx
+++ dada-app/src/App.jsx
@@ -7,20 +7,20 @@ import './index.css'
 
 //Importing context
 import GoalContextProvide from './context/GoalContext'
-import AuthContext from './context/AuthContext'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <AuthContext>
+      
         <GoalContextProvide>
           <Header />
           <Main />
           <Footer />
         </GoalContextProvide>
-      </AuthContext>
+    
     </div>
   )
 }
